93ce9e9c1fa7cb310caa4139fadd5779410c55ce
Vinicius Brian Bolzani <90060747+VBrianB@users.noreply.github.com>
2022-06-29 04:28:16 -0300

    Add files via upload

---
+++ Slider/script.js
@@ -0,0 +1,78 @@
+let TotalSlides = document.querySelectorAll('.slide').length;
+let currentSlide = 0;
+
+document.querySelector('.slideshow').style.width = `calc(100vw *${TotalSlides})`;
+
+
+
+
+function Previous(){
+    currentSlide--;
+    if(currentSlide < 0){
+        currentSlide = TotalSlides - 1;
+    }
+    updateDisplay();
+}
+
+function Next(){
+    currentSlide++
+    if(currentSlide > (TotalSlides -1 )){
+        currentSlide = 0;
+    }
+    updateDisplay();
+}
+
+
+
+function updateDisplay(){
+    SlideWidth = document.querySelector('#container').clientWidth;
+    NewMargin = (SlideWidth * currentSlide);
+    document.querySelector('.slideshow').style.marginLeft = `-${NewMargin}px`;
+    let posicaoSlide 
+    switch(currentSlide){
+        case 0:
+            posicaoSlide ='1';
+            break;
+        case 1:
+            posicaoSlide ='2';
+            break;
+        case 2:
+            posicaoSlide ='3';
+            break;
+    }
+    document.querySelector('.btn-navi-slide span').style.transform= 'translateY(225px)'
+    setTimeout(()=>{
+        document.querySelector('.btn-navi-slide span').innerHTML = posicaoSlide;
+    },200)
+    setTimeout(()=>{
+        document.querySelector('.btn-navi-slide span').style.opacity = 0
+        },100)  
+    setTimeout(()=>{
+    document.querySelector('.btn-navi-slide span').style.transform= 'translateY(-225px)'
+    },100)  
+    setTimeout(()=>{
+        document.querySelector('.btn-navi-slide span').style.transform= 'translateY(0px)'
+        },300)  
+    setTimeout(()=>{
+    document.querySelector('.btn-navi-slide span').style.opacity= 1;
+            },400)  
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function Search(){
+    let searchArea = document.querySelector('.search');
+    searchArea.classList.toggle('active');
+}
