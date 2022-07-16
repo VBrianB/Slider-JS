b25a632f089f53703d6792eeda6da6006bc67af5
Vinicius Brian Bolzani <90060747+VBrianB@users.noreply.github.com>
2022-07-16 08:10:46 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,42 @@
+## 👀 Overview
+
+Um silder criado em HTML, CSS e JAVASCRIPT para uma plataforma de trabalho para fotografos, onde você pode postar seus trabalhos e criar Network totalmente de graça
+
+
+<h2 align="center">Slide 1</h3>
+<img src="prints/print1.jpeg"> 
+<h2 align="center">Slide 2</h3>
+<img src="prints/print2.jpeg"> 
+<h2 align="center">Slide 3</h3>
+<img src="prints/print3.jpeg"> 
+
+
+
+## 💡 Ideia 
+
+Esse projeto foi criado para treinar efeito de slide que aprendi nas aulas do meu curso, para isso, criei uma página de boas-vindas para uma plataforma que funciona como uma rede social.
+O Angle é uma plataforma onde fotografos podem criar Network e postar seus trabalhos para o mundo todo, eu pretendo continuar esse projeto depois com um pouco mais de lógica, mas de inicio, o principal era criar um alide bem estrutura com JAVASCRIPT e utilizando de algumas animações.
+
+## ⚙️ Tools
+
+ - HTML
+ - CSS
+ - JAVASCRIPT
+
+
+<br>
+<br>
+
+## 🚀 Future
+<br> 
+
+### Código
+Como dito antes, este projeto provavelmente será levado para frente com um pouco mais de lógica, ou talvez seja recriado em React ou outro framework.
+
+<br>
+<br>
+
+<br>
+<br>
+<br>
+<h3 align="center"> Criado com muito ❤️ por <a href="https://github.com/VBrianB"> Vinicius Brian Bolzani</a></h2>
---
+++ script.js
@@ -0,0 +1,113 @@
+let TotalSlides = document.querySelectorAll('.slide').length;
+let currentSlide = 0;
+
+document.querySelector('.slideshow').style.width = `calc(100vw *${TotalSlides})`;
+
+
+
+
+function Previous(){
+    currentSlide--;
+    if(currentSlide < 0){
+        currentSlide = TotalSlides - 1;
+    }
+    updateDisplay();
+}
+
+function Next(){
+    currentSlide++
+    if(currentSlide > (TotalSlides -1 )){
+        currentSlide = 0;
+    }
+    updateDisplay();
+}
+
+
+
+
+
+
+
+
+
+
+function updateDisplay(){
+    SlideWidth = document.querySelector('#container').clientWidth;
+    NewMargin = (SlideWidth * currentSlide);
+    document.querySelector('.slideshow').style.marginLeft = `-${NewMargin}px`;
+    let posicaoSlide 
+    switch(currentSlide){
+        case 0:
+            posicaoSlide ='1';
+            document.querySelector('.btn-navi-slide .AL').style.display= 'none';
+            break;
+        case 1:
+            posicaoSlide ='2';
+            document.querySelector('.btn-navi-slide .AL').style.display= 'block';
+            break;
+        case 2:
+            posicaoSlide ='3';
+            document.querySelector('.btn-navi-slide .AL').style.display= 'block';
+            break;
+    }
+    document.querySelector('.btn-navi-slide span').style.transform= 'translateY(225px)'
+    setTimeout(()=>{
+        document.querySelector('.btn-navi-slide span').innerHTML = posicaoSlide;
+    },200)
+    setTimeout(()=>{
+        document.querySelector('.btn-navi-slide span').style.opacity = 0
+        },100)  
+    setTimeout(()=>{
+    document.querySelector('.btn-navi-slide span').style.transform= 'translateY(-225px)'
+    },100)  
+    setTimeout(()=>{
+        document.querySelector('.btn-navi-slide span').style.transform= 'translateY(0px)'
+        },300)  
+    setTimeout(()=>{
+    document.querySelector('.btn-navi-slide span').style.opacity= 1;
+            },400)  
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function Search(){
+    let searchArea = document.querySelector('.search');
+    searchArea.classList.toggle('active');
+}
+function Menu(){
+    let menu = document.querySelector('#menu');
+    let Content = document.querySelector('.content');
+    if(menu.style.width === '0px'){
+        setTimeout(()=>{
+            menu.style.width = '285px'; 
+            
+        },100);
+       menu.style.display = 'flex';
+
+       setTimeout(()=>{
+        Content.style.display = 'block'
+       },500);
+      
+       
+    }
+    else{
+        Content.style.display = 'none'
+        menu.style.width = '0px';
+        setTimeout(()=>{
+            menu.style.display = 'none';
+        },300)
+    }
+   
+}
